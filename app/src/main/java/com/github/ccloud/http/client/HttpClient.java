4f0c17bb6b58ce8324bda1b1144449ebc3b70da2
ford_ji <ford_ji@epam.com>
2023-02-01 18:07:02 +0800

    change url to host config

--- app/src/main/java/com/github/ccloud/http/client/HttpClient.java
+++ app/src/main/java/com/github/ccloud/http/client/HttpClient.java
@@ -1,6 +1,7 @@
 package com.github.ccloud.http.client;
 
-import com.github.ccloud.http.HttpConfig;
+import com.github.ccloud.constant.HostConstant;
+import com.github.ccloud.util.SpUtil;
 
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -11,9 +12,10 @@ public class HttpClient {
         //构建Retrofit实例
         Retrofit retrofit = new Retrofit.Builder()
                 //设置网络请求BaseUrl地址
-                .baseUrl(HttpConfig.API_HOST)
+                .baseUrl(SpUtil.getInstance().getString(HostConstant.HOST_ADDRESS_KEY, ""))
                 //设置数据解析器
                 .addConverterFactory(GsonConverterFactory.create())
+//todo customize okhttp client                .client()
                 .build();
 
         return retrofit.create(httpClass);
